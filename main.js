22d6274ac8156e7d5dd924dd69a24164e39e2ccc
nadeenabdulrazek <nadeenabdul711@gmail.com>
2024-12-09 02:09:11 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let img = document.querySelector('.img');
+let container = document.querySelector('.container');
+function perfumes(x){
+    img.src = x ;
+}
+function colors(y){
+    container.style.background = y;
+
+}
